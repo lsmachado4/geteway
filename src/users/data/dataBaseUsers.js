b48b157b72f91b3639082e673948c8aa485fbd54
Paula Santos <88747989+paula-santos-gb@users.noreply.github.com>
2022-10-20 23:12:10 -0300

    update databaseUsers file

--- src/users/data/dataBaseUsers.js
+++ src/users/data/dataBaseUsers.js
@@ -1,6 +1,14 @@
 const axios = require('axios')
 
-const dataBaseUsers = axios.create({ baseURL: "https://jsonplaceholder.typicode.com/users"})
+
+const getUsers = async () => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(response.data)
+    } catch (err) {
+     console.log(err);
+    }
+};
 
 
-module.exports = dataBaseUsers
+module.exports = getUsers()
